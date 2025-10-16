20488078d0f1110bed883ebc3a86359886ea13ae
Nguyen Van Cong <congnguyenvan522@gmail.com>
2025-10-17 00:46:29 +0700

    Update Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Product\CreateRequest;
+use App\Http\Requests\Product\UpdateRequest;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    public function update(UpdateRequest $request,$id){
+        $product=Product::findOrFail($id);
+        $data=$request->validated();
+        $product->update($data);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Updated successfully',
+            'data' => new ProductResource($product)
+        ]);
+    }
     public function store(CreateRequest $request)
     {
         $userRequest = $request->validated();
