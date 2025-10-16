92712374e6950d13ac98e234ce20a8ac093ebb0d
XuanHoang1505 <ngokhong15052004@gmail.com>
2025-10-16 08:37:36 +0700

    Nguyen Xuan Hoang create update product

---
+++ app/Http/Requests/Product/UpdateProductRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests\Product;
+
+use App\Http\Requests\BaseRequest;
+
+class UpdateProductRequest extends BaseRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'nullable|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'nullable|numeric|min:0',
+        ];
+    }
+}
