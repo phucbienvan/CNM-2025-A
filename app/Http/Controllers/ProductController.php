20511fc3148219f53f2ed7abf6279faf073b77d3
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-10-21 23:43:40 +0700

    add function update product in productController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\Product\CreateRequest;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Http\Requests\Product\UpdateRequest;
 
 class ProductController extends Controller
 {
@@ -22,5 +23,25 @@ class ProductController extends Controller
         $products = Product::orderBy('id', 'desc');
 
         return ProductResource::apiPaginate($products, $request);
+    }
+    public function update(UpdateRequest $request, $id)
+    {
+        //dd('123');
+        $userRequest = $request->validated();
+        $product = Product::findOrFail($id);
+       $dataToUpdate = [];
+        if(isset($userRequest['name'])) {
+            $dataToUpdate['name'] = $userRequest['name'];
+        }
+        if(isset($userRequest['description'])) {
+            $dataToUpdate['description'] = $userRequest['description'];
+        }
+        if(isset($userRequest['price'])) {
+            $dataToUpdate['price'] = $userRequest['price'];
+        }
+        if(!empty($dataToUpdate)) {
+            $product->update($dataToUpdate); //nếu có dữ liệu để cập nhật thì mới gọi hàm update
+        } 
+        return new ProductResource($product);
     }
 }
