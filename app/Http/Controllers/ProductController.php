c85392ee95d79b11b7dcd4ce6c2a485dd14dc9ab
DinhDucNguyen <ducdinh127855@gmail.com>
2025-10-21 22:19:03 +0700

    thêm func update product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Product\CreateRequest;
+use App\Http\Requests\Product\UpdateRequest;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
@@ -23,4 +24,13 @@ class ProductController extends Controller
 
         return ProductResource::apiPaginate($products, $request);
     }
+
+    public function update(UpdateRequest $request, Product $product)
+    {
+        $validated = $request->validated();
+        $product->update($validated);
+
+        return new ProductResource($product);
+    }
+
 }
