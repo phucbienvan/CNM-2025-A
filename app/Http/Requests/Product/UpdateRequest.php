1d3380d8af76b1e6b85f8ff9157a748d99da4f96
heri <hoangdinhlamhai1@gmail.com>
2025-10-22 08:53:19 +0700

    Lam Hai update product

---
+++ app/Http/Requests/Product/UpdateRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests\Product;
+
+use App\Http\Requests\BaseRequest;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends BaseRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'sometimes|required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'sometimes|required|numeric|min:0',
+        ];
+    }
+}
