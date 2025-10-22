4f85dc9252ff49dfc9fd36b993f98d9ca617b5bc
AnhDuy Phan <padanhduy06@gmail.com>
2025-10-22 18:55:51 +0700

    PhanAnhDuy_updateProduct

---
+++ app/Http/Requests/Product/UpdateRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Product;
+
+use App\Http\Requests\BaseRequest;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends BaseRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'sometimes|required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'sometimes|required|numeric|min:0',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'price.numeric' => 'Price must be number',
+            'name.required' => 'Name is required',
+            'description.required' => 'Description is required',
+            'price.required' => 'Price is required',
+            'name.string' => 'Name must be string',
+            'description.string' => 'Description must be string',
+            'name.max' => 'Name must be less than 255 characters',
+        ];
+    }
+}
