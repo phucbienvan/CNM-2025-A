9f57691caa3a697c830f2684d66acdd75060ea38
Nguyen Van Hai <nguyenvanhai.pg1610@gmail.com>
2025-11-17 21:32:08 +0700

    Van Hai resend code

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegisterRequest;
+use App\Http\Requests\Auth\VerifyCodeRequest;
 use App\Http\Resources\UserResource;
+use App\Mail\SendMail;
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 
 class AuthController extends Controller
 {
@@ -15,11 +18,72 @@ class AuthController extends Controller
     {
         $userRequest = $request->validated();
         $userRequest['password'] = Hash::make($userRequest['password']);
+
+        $code = rand(100000, 999999);
+        $expiredCodeAt = now()->addMinutes(10);
+        $userRequest['verify_code'] = $code;
+        $userRequest['expired_code_at'] = $expiredCodeAt;
+        $userRequest['status'] = 0;
         $user = User::create($userRequest);
+
+        Mail::to($user->email)->send(new SendMail($code));
 
         return response()->json([
             'message' => 'User created successfully',
         ], 201);
+    }
+
+    public function verifyCode(VerifyCodeRequest $request)
+    {
+        $userRequest = $request->validated();
+        $user = User::where('email', $userRequest['email'])->first();
+
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found',
+            ], 404);
+        }
+
+        if ($user->expired_code_at < now()) {
+            return response()->json([
+                'message' => 'Code has expired',
+            ], 400);
+        }
+
+        if ($user->verify_code !== $userRequest['code']) {
+            return response()->json([
+                'message' => 'Code is incorrect',
+            ], 400);
+        }
+
+        $user->status = 1; // 1: verified, 0: not verified
+        $user->save();
+
+        return response()->json([
+            'message' => 'Code verified successfully',
+        ], 200);
+    }
+
+    public function resendCode(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found',
+            ], 404);
+        }
+
+        $code = rand(100000, 999999);
+        $expiredCodeAt = now()->addMinutes(10);
+        $user->verify_code = $code;
+        $user->expired_code_at = $expiredCodeAt;
+        $user->save();
+
+        Mail::to($user->email)->send(new SendMail($code));
+
+        return response()->json([
+            'message' => 'Code resent successfully',
+        ], 200);
     }
 
     public function login(LoginRequest $request)
@@ -33,9 +97,14 @@ class AuthController extends Controller
                 'message' => 'Password or email is incorrect',
             ], 401);
         }
+        if (!$user || $user->status !== 1) {
+            return response()->json([
+                'message' => 'User not verified',
+            ], 401);
+        }
 
         $accessToken = $user->createToken('auth_token')->plainTextToken;
-        
+
         return response()->json([
             'user' => new UserResource($user),
             'message' => 'Login successful',
