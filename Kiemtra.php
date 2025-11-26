26c12ffdb2d32cea8e20024c515e10ddadd90fad
bienvanphuc <phucbv@flydinotech.com>
2025-11-26 19:12:32 +0700

    chua bai

--- Kiemtra.php
+++ Kiemtra.php
@@ -7,30 +7,39 @@ use App\Models\Category;
 use App\Models\Permission;
 
 class UserService {
-    public function __construct(User $user,
-        Category $category, Permission $permission) {
+    public function __construct(
+        User $user,
+        Category $category, 
+        Permission $permission
+    ) {
         $this->user = $user;
         $this->category = $category;
         $this->permission = $permission;
     }
 
-    public function create_user($param)
+    public function createUser($params)
     {
         $user = [
-            'name' => $param['name'],
-            'email' => $param['email']
+            'name' => $params['name'],
+            'email' => $params['email']
         ];
-        if(isset($param['password'] && $param['password'])){
-        	$user['password'] = $param['password']
+
+        if(isset($params['password'])){
+        	$user['password'] = bcrypt($params['password']);
         }
+
         return $this->user->create($user);
     }
 
     public function search($param){
-    	$UserSearch = $this->user;
+    	$userSearch = $this->user;
+
     	if ($param['keyword']) {
-    		$UserSearch->where('name', "like","%$param['keyword']");
+    		$userSearch->where('name', 'like', "%$param['keyword']");
     	}
-    	return $UserSearch->with("role",'group')->where('status', true)->whereHas('schedule')->paginate();
+
+    	return $userSearch->with('role','group')
+            ->where('status', true)
+            ->whereHas('schedule')->paginate(10);
     }
 }
